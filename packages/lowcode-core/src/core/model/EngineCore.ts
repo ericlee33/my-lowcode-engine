e603474954e75edbf407abc6126cba27c95e6afb
李世豪 <lishihao.destiny@bytedance.com>
2024-12-04 19:27:37 +0800

    feat: remove drag debounce

--- packages/lowcode-core/src/core/model/EngineCore.ts
+++ packages/lowcode-core/src/core/model/EngineCore.ts
@@ -1,103 +1,112 @@
 import { makeAutoObservable, reaction } from 'mobx';
 import { generateId } from '../../utils';
-import { find, map, findIndex } from 'lodash-es';
+import { find, map, findIndex, forEach } from 'lodash-es';
 import { $$_editor_json_schema } from '../../Editor/constants/cache';
 
 export type Element = {
-	/** uuid */
-	id: string;
-	/** widget type */
-	type: string;
-	props?: Record<string, any>;
-	children: Element[];
+  /** uuid */
+  id: string;
+  /** widget type */
+  type: string;
+  props?: Record<string, any>;
+  children: Element[];
 };
 
 type SchemaControllerProps = {
-	schema?: Element[];
+  schema?: Element[];
 };
 
 class EngineCore {
-	static DefaultSchema = [
-		{
-			type: 'page',
-			id: generateId(),
-			children: [],
-		},
-	];
+  static DefaultSchema = [
+    {
+      type: 'page',
+      id: generateId(),
+      children: [],
+    },
+  ];
 
-	get schmea() {
-		return this.$schema;
-	}
+  get schmea() {
+    return this.$schema;
+  }
 
-	private $schema: Element[];
+  private $schema: Element[];
+  private $disposers = [];
 
-	constructor(props: SchemaControllerProps) {
-		makeAutoObservable(this);
-		console.log(props, 24);
-		this.$schema = props.schema ?? EngineCore.DefaultSchema;
+  constructor(props: SchemaControllerProps) {
+    makeAutoObservable(this);
+    this.$schema = props.schema ?? EngineCore.DefaultSchema;
 
-		const disposer = reaction(
-			() => {
-				return JSON.stringify(this.$schema);
-			},
-			($schema) => {
-				console.log($schema, '4242');
-				localStorage.setItem($$_editor_json_schema, $schema);
-			}
-		);
-	}
+    const disposer = reaction(
+      () => {
+        return JSON.stringify(this.$schema);
+      },
+      ($schema) => {
+        localStorage.setItem($$_editor_json_schema, $schema);
+      }
+    );
 
-	has(id: string) {
-		return !!this.traverse(this.$schema, id);
-	}
+    this.$disposers.push(disposer);
+  }
 
-	add(compoent: Element, id: string) {
-		const [target] = this.traverse(this.$schema, id);
-		target.children.push(compoent);
-	}
+  has(id: string) {
+    return !!this.traverse(this.$schema, id);
+  }
 
-	remove(id: string) {
-		return this.$remove(this.$schema, id);
-	}
+  add(compoent: Element, id: string) {
+    const [target] = this.traverse(this.$schema, id);
+    target.children.push(compoent);
+  }
 
-	swap(aId: string, bId: string) {
-		const [eleA, aFatherNode] = this.traverse(this.$schema, aId);
-		const [eleB, bFatherNode] = this.traverse(this.$schema, bId);
+  remove(id: string) {
+    return this.$remove(this.$schema, id);
+  }
 
-		const aIdx = aFatherNode.findIndex((item) => item.id === aId);
-		// console.log(aId, bId, aFatherNode, bFatherNode, 44);
-		aFatherNode.splice(aIdx, 1, eleB);
-		const bIdx = bFatherNode.findIndex((item) => item.id === bId);
-		bFatherNode.splice(bIdx, 1, eleA);
-	}
+  swap(aId: string, bId: string) {
+    const [eleA, aFatherNode] = this.traverse(this.$schema, aId);
+    const [eleB] = this.traverse(this.$schema, bId);
 
-	$remove(children: Element[], id: string) {
-		const compoentIndex = findIndex(children, (compoent) => compoent.id === id);
-		if (compoentIndex > -1) {
-			console.log(compoentIndex, children);
-			return children.splice(compoentIndex, 1);
-		}
-		for (const component of children) {
-			return this.$remove(component.children, id);
-		}
+    const aIdx = aFatherNode.findIndex((item) => item.id === aId);
+    aFatherNode.splice(aIdx, 1, eleB);
+    const [, bFatherNode] = this.traverse(this.$schema, bId);
 
-		console.error('没有找到 remove 元素');
-	}
+    console.log(eleA, eleB, 'elemntA');
+    const bIdx = bFatherNode.findIndex((item) => item.id === bId);
+    bFatherNode.splice(bIdx, 1, eleA);
+    console.log(bFatherNode, 'bFatherNode');
+  }
 
-	private traverse(children: Element[], id: string): [Element, Element[]] {
-		const compoent = find(children, (compoent) => compoent.id === id);
-		if (compoent) {
-			return [compoent, children];
-		}
+  $remove(children: Element[], id: string) {
+    const compoentIndex = findIndex(children, (compoent) => compoent.id === id);
+    if (compoentIndex > -1) {
+      return children.splice(compoentIndex, 1);
+    }
+    for (const component of children) {
+      return this.$remove(component.children, id);
+    }
 
-		for (const component of children) {
-			return this.traverse(component.children, id);
-		}
-	}
+    console.error('没有找到 remove 元素');
+  }
 
-	reset() {
-		this.$schema = EngineCore.DefaultSchema;
-	}
+  private traverse(children: Element[], id: string): [Element, Element[]] {
+    const compoent = find(children, (compoent) => compoent.id === id);
+    if (compoent) {
+      return [compoent, children];
+    }
+
+    for (const component of children) {
+      return this.traverse(component.children, id);
+    }
+  }
+
+  reset() {
+    this.$schema = EngineCore.DefaultSchema;
+  }
+
+  destroy() {
+    forEach(this.$disposers, (disposer) => {
+      disposer();
+    });
+  }
 }
 
 export default EngineCore;
