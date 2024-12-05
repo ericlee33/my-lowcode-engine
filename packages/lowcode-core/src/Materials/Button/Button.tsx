8c8095ac5cece09f48f2de28f12476344c197afd
李世豪 <lishihao.destiny@bytedance.com>
2024-12-05 19:04:31 +0800

    fix: drag issue

--- packages/lowcode-core/src/Materials/Button/Button.tsx
+++ packages/lowcode-core/src/Materials/Button/Button.tsx
@@ -12,148 +12,150 @@ import { useDrag } from 'react-dnd';
 import { generateId } from '../../utils';
 
 interface IButtonProps extends ButtonProps {
-	className?: string;
-	style?: React.CSSProperties;
-	text?: string;
-	id: string;
-	parentId: string;
-	ref: React.ForwardedRef<ThisType<typeof Button>>;
-	forwardedRef: React.ForwardedRef<ThisType<typeof Button>>;
-	engineCore: EngineCore;
+  className?: string;
+  style?: React.CSSProperties;
+  text?: string;
+  id: string;
+  parentId: string;
+  ref: React.ForwardedRef<ThisType<typeof Button>>;
+  forwardedRef: React.ForwardedRef<ThisType<typeof Button>>;
+  engineCore: EngineCore;
 }
 
 const Root = styled(ArcoButton)``;
 
 const Button: React.FC<IButtonProps> = ({
-	className,
-	style,
-	text,
-	forwardedRef,
-	id,
-	engineCore,
-	parentId,
-	...props
+  className,
+  style,
+  text,
+  forwardedRef,
+  id,
+  engineCore,
+  parentId,
+  ...props
 }) => {
-	const nodeRef = useRef();
+  const nodeRef = useRef();
 
-	// const [render, setRender] = useState(false);
-	// const { schemaConfig } = useSchemaContext();
-	// const { componentContext } = useScoped();
+  // const [render, setRender] = useState(false);
+  // const { schemaConfig } = useSchemaContext();
+  // const { componentContext } = useScoped();
 
-	// const switchChildNodeRender = () => {
-	// 	setRender((render) => !render);
-	// };
+  // const switchChildNodeRender = () => {
+  // 	setRender((render) => !render);
+  // };
 
-	// useImperativeHandle(forwardedRef, () => ({
-	// 	switchChildNodeRender,
-	// }));
+  // useImperativeHandle(forwardedRef, () => ({
+  // 	switchChildNodeRender,
+  // }));
 
-	// const onClick = (event) => {
-	// 	dispatchEvent({
-	// 		event,
-	// 		id,
-	// 		schemaConfig,
-	// 		componentContext: componentContext.current,
-	// 	});
-	// };
+  // const onClick = (event) => {
+  // 	dispatchEvent({
+  // 		event,
+  // 		id,
+  // 		schemaConfig,
+  // 		componentContext: componentContext.current,
+  // 	});
+  // };
 
-	const [{ isDragging, opacity }, drag] = useDrag(
-		() => ({
-			type: ItemTypes.BOX,
-			// 传递的信息
-			item: () => ({ type: ButtonMeta.type, id: id, children: [], parentId }),
-			end: (item, monitor) => {
-				// 获取 drop 通过 drop 回调 return 的数据
-				const dropResult = monitor.getDropResult();
-				if (!monitor.didDrop()) {
-					// console.log(item.id, 'ididd');
-					engineCore.remove(item.id);
-				}
-				if (item && dropResult) {
-					// console.log(item, 333, dropResult);
-				}
-			},
-			canDrag: true,
-			collect: (monitor) => ({
-				isDragging: monitor.isDragging(),
-				handlerId: monitor.getHandlerId(),
-				opacity: monitor.isDragging() ? 0 : 1,
-			}),
-		}),
-		[id]
-	);
+  const [{ isDragging, opacity }, drag] = useDrag(
+    () => ({
+      type: ItemTypes.BOX,
+      // 传递的信息
+      item: () => ({ type: ButtonMeta.type, id: id, children: [], parentId }),
+      end: (item, monitor) => {
+        // 获取 drop 通过 drop 回调 return 的数据
+        const dropResult = monitor.getDropResult();
+        if (!monitor.didDrop()) {
+          // console.log(item.id, 'ididd');
+          engineCore.remove(item.id);
+        }
+        if (item && dropResult) {
+          // console.log(item, 333, dropResult);
+        }
+      },
+      canDrag: true,
+      collect: (monitor) => ({
+        isDragging: monitor.isDragging(),
+        handlerId: monitor.getHandlerId(),
+        opacity: monitor.isDragging() ? 0 : 1,
+      }),
+    }),
+    [id]
+  );
 
-	const [{ canDrop, isOver }, { nodeRef: _nodeRef }] = useDrop({
-		accept: ItemTypes.BOX,
-		deps: [engineCore, id],
-		moveCard: (
-			/** 抛来的 element */
-			element: Element,
-			/** 当前被释放元素的 id */
-			id: string
-		) => {
-			const hasElement = engineCore.has(element.id);
-			if (element.id === id) {
-				// return engineCore.remove(element.id);
-				return;
-			}
+  const [{ canDrop, isOver }, { nodeRef: _nodeRef }] = useDrop({
+    accept: ItemTypes.BOX,
+    deps: [engineCore, id],
+    moveCard: (
+      /** 抛来的 element */
+      element: Element,
+      /** 当前被释放元素的 id */
+      id: string
+    ) => {
+      const hasElement = engineCore.has(element.id);
+      if (element.id === id) {
+        // return engineCore.remove(element.id);
+        return;
+      }
 
-			if (!hasElement) {
-				engineCore.add({ ...element, parentId }, id);
-			} else {
-				// parent 相同，需要特殊处理
-				if (element.parentId === parentId) {
-					const parent = engineCore.get(element.parentId);
-					const dropIdx = parent.children.findIndex((item) => item.id === id);
-					const dragIdx = parent.children.findIndex(
-						(item) => item.id === element.id
-					);
-					engineCore.remove(element.id);
+      if (!hasElement) {
+        engineCore.add({ ...element, parentId }, id);
+      } else {
+        // parent 相同，需要特殊处理
+        if (element.parentId === parentId) {
+          const parent = engineCore.get(element.parentId);
+          const dropIdx = parent.children.findIndex((item) => item.id === id);
+          const dragIdx = parent.children.findIndex(
+            (item) => item.id === element.id
+          );
+          engineCore.remove(element.id);
 
-					if (dragIdx < dropIdx) {
-						engineCore.insertAfterParentIdx(
-							element,
-							parent.children,
-							dropIdx + 1
-						);
-					} else {
-						engineCore.insertAfterParentIdx(element, parent.children, dropIdx);
-					}
-					return;
-				}
-				// 删除
-				engineCore.remove(element.id);
-				// 插入
-				engineCore.insertAfter(element, id);
-			}
-		},
-		ref: nodeRef,
-		id: id,
-	});
+          // 由于先删掉了，如果拖的元素在前面，需要补齐 index
+          // if (dragIdx < dropIdx) {
+          if (dragIdx + 1 < dropIdx) {
+            engineCore.insertAfterParentIdx(
+              element,
+              parent.children,
+              dropIdx + 1
+            );
+          } else {
+            engineCore.insertAfterParentIdx(element, parent.children, dropIdx);
+          }
+          return;
+        }
+        // 删除
+        engineCore.remove(element.id);
+        // 插入
+        engineCore.insertAfter(element, id);
+      }
+    },
+    ref: nodeRef,
+    id: id,
+  });
 
-	return (
-		<div
-			style={{
-				marginBottom: 10,
-				// opacity: opacity,
-				border: isOver ? '1px solid blue' : '',
-			}}
-			ref={drag(_nodeRef)}
-		>
-			<div
-				onClick={(e) => {
-					e.preventDefault();
-				}}
-				className={className}
-				style={style}
-				{...props}
-				// onClick={onClick}
-			>
-				{text || `默认值${id}`}
-				{/* {render ? '隐藏的孩子' : ''} */}
-			</div>
-		</div>
-	);
+  return (
+    <div
+      style={{
+        marginBottom: 10,
+        // opacity: opacity,
+        border: isOver ? '1px solid blue' : '',
+      }}
+      ref={drag(_nodeRef)}
+    >
+      <ArcoButton
+        onClick={(e) => {
+          e.preventDefault();
+        }}
+        className={className}
+        style={{ ...style, pointerEvents: 'none' }}
+        {...props}
+        // onClick={onClick}
+      >
+        {text || `默认值${id}`}
+        {/* {render ? '隐藏的孩子' : ''} */}
+      </ArcoButton>
+    </div>
+  );
 };
 
 export default Button;
--- packages/lowcode-core/src/Materials/Page/Page.tsx
+++ packages/lowcode-core/src/Materials/Page/Page.tsx
@@ -5,61 +5,56 @@ import EngineCore, { Element } from '../../core/model/EngineCore';
 import { useDrop } from '../../Editor/hooks/useDrop';
 
 interface IContainerProps {
-	className?: string;
-	style?: React.CSSProperties;
-	engineCore: EngineCore;
-	id: string;
+  className?: string;
+  style?: React.CSSProperties;
+  engineCore: EngineCore;
+  id: string;
 }
 
 const Root = styled.div`
-	border: 1px solid #e1e1e1;
-	height: 100vh;
-	padding: 10px;
-	background-color: gainsboro;
+  border: 1px solid #e1e1e1;
+  height: 100vh;
+  padding: 10px;
+  background-color: gainsboro;
 `;
 
 const Page = forwardRef<{}, IContainerProps>((props, ref) => {
-	const nodeRef = useRef();
-	const { className, style, children, engineCore, id } = props;
+  const nodeRef = useRef();
+  const { className, style, children, engineCore, id } = props;
 
-	// -> hover 既放置，接下来做排序
-	const [{ canDrop, isOver }, { nodeRef: _nodeRef }] = useDrop({
-		accept: ItemTypes.BOX,
-		deps: [engineCore],
-		moveCard: (element: Element, id: string) => {
-			/**
-			 * 1、如果此时拖拽的组件是 Box 组件，则 dragIndex 为 undefined，则此时修改，则此时修改 cardList 中的占位元素的位置即可
-			 * 2、如果此时拖拽的组件是 Card 组件，则 dragIndex 不为 undefined，此时替换 dragIndex 和 hoverIndex 位置的元素即可
-			 */
-			const hasElement = engineCore.has(element.id);
+  // -> hover 既放置，接下来做排序
+  const [{ canDrop, isOver }, { nodeRef: _nodeRef }] = useDrop({
+    accept: ItemTypes.BOX,
+    deps: [engineCore],
+    moveCard: (element: Element, id: string) => {
+      /**
+       * 1、如果此时拖拽的组件是 Box 组件，则 dragIndex 为 undefined，则此时修改，则此时修改 cardList 中的占位元素的位置即可
+       * 2、如果此时拖拽的组件是 Card 组件，则 dragIndex 不为 undefined，此时替换 dragIndex 和 hoverIndex 位置的元素即可
+       */
+      const hasElement = engineCore.has(element.id);
 
-			if (!hasElement) {
-				console.log(id, 'iddd');
-				engineCore.add({ ...element, parentId: id }, id);
-			} else {
-			}
-		},
-		ref: nodeRef,
-		id,
-	});
+      if (!hasElement) {
+        engineCore.add({ ...element, parentId: id }, id);
+        // 为拖拽中的元素注入 parentId，避免在拽到 button 移动顺序之后之后不符合预期
+        element.parentId = id;
+      }
+    },
+    ref: nodeRef,
+    id,
+  });
 
-	return (
-		<Root
-			ref={_nodeRef}
-			className={className}
-			style={style}
-			{...props}
-		>
-			{children}
-		</Root>
-	);
+  return (
+    <Root ref={_nodeRef} className={className} style={style} {...props}>
+      {children}
+    </Root>
+  );
 });
 
 Page.actions = [
-	{
-		label: '清空',
-		value: 'onClear',
-	},
+  {
+    label: '清空',
+    value: 'onClear',
+  },
 ];
 
 export default Page;
