13322e46a23b1d2f6b4db5d47d9325d5e3415a09
ericlee33 <ericfly33@gmail.com>
2024-12-25 10:03:00 +0800

    fix: bugfix

--- packages/lowcode-core/src/Materials/Table/component.tsx
+++ packages/lowcode-core/src/Materials/Table/component.tsx
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-import React, { useMemo } from 'react';
-=======
 import React, { useEffect, useMemo } from 'react';
->>>>>>> Stashed changes
 import styled from 'styled-components';
 import { Empty, Table, TableProps } from '@arco-design/web-react';
 import { ElementProps } from '../../renderer/types/element';
@@ -12,28 +8,9 @@ interface ITableProps extends ElementProps<TableProps> {}
 const Root = styled(Table)``;
 
 const TableComponent: React.FC<ITableProps> = (props) => {
-  const { componentConfig } = props;
-  const { columns, data } = componentConfig ?? {};
+	const { componentConfig } = props;
+	const { columns, data } = componentConfig ?? {};
 
-<<<<<<< Updated upstream
-  const tableData = useMemo(() => {
-    if (!data || !Array.isArray(data)) {
-      return [];
-    }
-
-    return data;
-  }, [data]);
-
-  return columns ? (
-    <Root columns={columns} data={tableData} />
-  ) : (
-    // <Root
-    // 	columns={columns}
-    // 	data={[]}
-    // />
-    <Empty description="请先定义列" />
-  );
-=======
 	const tableData = useMemo(() => {
 		if (Array.isArray(data)) {
 			return data;
@@ -54,7 +31,6 @@ const TableComponent: React.FC<ITableProps> = (props) => {
 	) : (
 		<Empty description="请先定义列" />
 	);
->>>>>>> Stashed changes
 };
 
 export default TableComponent;
